077da67a663c80f895bdff77eafeadbd9ff8a21e
Suresh G <sureshg.g@gmail.com>
2021-07-18 16:46:50 -0700

    feat: kotlin scripting sample

---
+++ script.main.kts
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S kotlin -Xplugin=/usr/local/Cellar/kotlin/1.5.21/libexec/lib/kotlinx-serialization-compiler-plugin.jar
+
+// @file:Repository("https://maven.google.com")
+@file:DependsOn("io.ktor:ktor-client-core:1.6.1")
+@file:DependsOn("io.ktor:ktor-client-cio:1.6.1")
+@file:DependsOn("io.ktor:ktor-client-java:1.6.1")
+@file:DependsOn("io.ktor:ktor-client-auth:1.6.1")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
+@file:DependsOn("org.slf4j:slf4j-simple:2.0.0-alpha2")
+@file:DependsOn("com.microsoft.playwright:playwright:1.12.1")
+// @file:Import("common.main.kts")
+
+@file:CompilerOptions("-Xopt-in=kotlin.RequiresOptIn", "-jvm-target", "1.8")
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+import com.microsoft.playwright.*
+import kotlinx.coroutines.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class Lang(val name: String)
+
+val arg = args.firstOrNull() ?: "Kotlin"
+println("Hello $arg!")
+
+val serialized = Json.encodeToString(Lang("Kotlin"))
+println(serialized)
+
+val deserialized = Json.decodeFromString<Lang>("""{"name" : "Java"}""")
+println(deserialized)
+
+runBlocking {
+  println("Hi")
+  delay(1000)
+  println("Done!")
+}
+
+fun recordBrowser() {
+  CLI.main(arrayOf("codegen", "-o", "record.java"))
+}
+
+fun playbackBrowser() {
+  Playwright.create().use { playwright ->
+    val browser = playwright.chromium().launch(
+      BrowserType.LaunchOptions().setHeadless(true)
+    )
+
+    browser.newContext().newPage().apply {
+      navigate("https://www.google.com/search?q=kotlinlang&oq=kotlinlang")
+      click("text=Kotlin Programming Language")
+      assert(url() == "https://kotlinlang.org/")
+      click("#kotlin-code-example-simplest div div div div")
+      click("text=[X]")
+      close()
+    }
+  }
+}
+
+
+
+
+
