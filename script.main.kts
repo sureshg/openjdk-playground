c787fa0b2b09f6e1a6c6cf1a1747110d9bd53389
Suresh G <sureshg.g@gmail.com>
2021-08-25 08:05:06 -0700

    chore: kotlin script changes for 1.5.30

--- script.main.kts
+++ script.main.kts
@@ -1,19 +1,19 @@
-#!/usr/bin/env -S kotlin -Xplugin=/usr/local/Cellar/kotlin/1.5.21/libexec/lib/kotlinx-serialization-compiler-plugin.jar
+#!/usr/bin/env -S kotlin -Xplugin=/usr/local/Cellar/kotlin/1.5.30/libexec/lib/kotlinx-serialization-compiler-plugin.jar
 
 // @file:Repository("https://maven.google.com")
-@file:DependsOn("io.ktor:ktor-client-core:1.6.1")
-@file:DependsOn("io.ktor:ktor-client-cio:1.6.1")
-@file:DependsOn("io.ktor:ktor-client-java:1.6.1")
-@file:DependsOn("io.ktor:ktor-client-auth:1.6.1")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+@file:DependsOn("io.ktor:ktor-client-core:1.6.2")
+@file:DependsOn("io.ktor:ktor-client-cio:1.6.2")
+@file:DependsOn("io.ktor:ktor-client-java:1.6.2")
+@file:DependsOn("io.ktor:ktor-client-auth:1.6.2")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
-@file:DependsOn("org.slf4j:slf4j-simple:2.0.0-alpha2")
-@file:DependsOn("com.microsoft.playwright:playwright:1.12.1")
+@file:DependsOn("org.slf4j:slf4j-simple:2.0.0-alpha4")
+@file:DependsOn("com.microsoft.playwright:playwright:1.14.1")
 // @file:Import("common.main.kts")
 
 @file:CompilerOptions("-Xopt-in=kotlin.RequiresOptIn", "-jvm-target", "1.8")
-@file:OptIn(ExperimentalCoroutinesApi::class)
+@file:OptIn(ExperimentalCoroutinesApi::class, ExperimentalSerializationApi::class)
 
 import com.microsoft.playwright.*
 import kotlinx.coroutines.*
@@ -23,7 +23,7 @@ import kotlinx.serialization.json.*
 @Serializable
 data class Lang(val name: String)
 
-val arg = args.firstOrNull() ?: "Kotlin"
+val arg = args.firstOrNull() ?: "Kotlin ${KotlinVersion.CURRENT}"
 println("Hello $arg!")
 
 val serialized = Json.encodeToString(Lang("Kotlin"))
@@ -58,8 +58,3 @@ fun playbackBrowser() {
     }
   }
 }
-
-
-
-
-
