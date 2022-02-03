92f69fb58f51c1077abbeb26da73c2a946fb0120
Suresh G <sureshg.g@gmail.com>
2022-02-03 06:14:08 -0800

    chore: version catalog update plugin

--- src/main/kotlin/dev/suresh/concurrent/CoroutineTest.kt
+++ src/main/kotlin/dev/suresh/concurrent/CoroutineTest.kt
@@ -6,10 +6,10 @@ import kotlin.coroutines.*
 object CoError : Throwable("error")
 
 suspend fun main() {
-
-  val s = suspendCancellableCoroutine<Int> { cont ->
+  val num = suspendCancellableCoroutine<Int> { cont ->
     cont.resume(100) {
     }
     cont.resumeWithException(CoError)
+    cont.invokeOnCancellation { }
   }
 }
