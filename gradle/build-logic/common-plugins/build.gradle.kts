1d6b9449da8e6631e11f2cd4720665a458de00e7
Suresh G <sureshg.g@gmail.com>
2023-09-09 23:37:24 -0700

    chore: kotlin 1.9.20-Beta update

--- gradle/build-logic/common-plugins/build.gradle.kts
+++ gradle/build-logic/common-plugins/build.gradle.kts
@@ -25,7 +25,6 @@ tasks {
 kotlin {
   sourceSets.all {
     languageSettings.apply {
-      progressiveMode = true
       optIn("kotlin.ExperimentalStdlibApi")
       optIn("kotlin.io.path.ExperimentalPathApi")
       optIn("kotlin.time.ExperimentalTime")
